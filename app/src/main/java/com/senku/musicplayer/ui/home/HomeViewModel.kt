7aaf382b97d4e9fc9cc20676c64fcb2cabd73695
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:50:50 +0600

    Add real song loading ViewModel

--- app/src/main/java/com/senku/musicplayer/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/senku/musicplayer/ui/home/HomeViewModel.kt
@@ -1,5 +1,34 @@
 package com.senku.musicplayer.ui.home
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.senku.musicplayer.data.model.Song
+import com.senku.musicplayer.utils.MusicScanner
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel()
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
+
+    val songs = mutableStateListOf<Song>()
+
+    var isLoading = true
+
+    init {
+        loadSongs()
+    }
+
+    private fun loadSongs() {
+        viewModelScope.launch(Dispatchers.IO) {
+
+            val scanner = MusicScanner(getApplication())
+            val musicList = scanner.getAllSongs()
+
+            songs.clear()
+            songs.addAll(musicList)
+
+            isLoading = false
+        }
+    }
+}
