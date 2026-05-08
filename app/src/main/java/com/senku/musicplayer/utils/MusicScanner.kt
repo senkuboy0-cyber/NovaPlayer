6140fc2dff836037a5206b49c0be5ae5769b87a8
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:50:25 +0600

    Add real MediaStore music scanner

--- app/src/main/java/com/senku/musicplayer/utils/MusicScanner.kt
+++ app/src/main/java/com/senku/musicplayer/utils/MusicScanner.kt
@@ -1,5 +1,66 @@
 package com.senku.musicplayer.utils
 
+import android.content.ContentUris
 import android.content.Context
+import android.provider.MediaStore
+import com.senku.musicplayer.data.model.Song
 
-class MusicScanner(private val context: Context)
+class MusicScanner(private val context: Context) {
+
+    fun getAllSongs(): List<Song> {
+
+        val songs = mutableListOf<Song>()
+
+        val collection = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DURATION
+        )
+
+        val sortOrder = "${MediaStore.Audio.Media.TITLE} ASC"
+
+        context.contentResolver.query(
+            collection,
+            projection,
+            null,
+            null,
+            sortOrder
+        )?.use { cursor ->
+
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val albumColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)
+            val durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+
+            while (cursor.moveToNext()) {
+
+                val id = cursor.getLong(idColumn)
+                val title = cursor.getString(titleColumn)
+                val artist = cursor.getString(artistColumn)
+                val album = cursor.getString(albumColumn)
+                val duration = cursor.getLong(durationColumn)
+
+                val contentUri = ContentUris.withAppendedId(collection, id)
+
+                songs.add(
+                    Song(
+                        id = id,
+                        title = title,
+                        artist = artist,
+                        album = album,
+                        duration = duration,
+                        uri = contentUri,
+                        albumArt = null
+                    )
+                )
+            }
+        }
+
+        return songs
+    }
+}
