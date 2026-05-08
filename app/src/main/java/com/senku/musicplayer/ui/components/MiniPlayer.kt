dabf92fffc64a11156f2da042db0f9fea479f571
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:25:12 +0600

    Add MiniPlayer

---
+++ app/src/main/java/com/senku/musicplayer/ui/components/MiniPlayer.kt
@@ -0,0 +1,9 @@
+package com.senku.musicplayer.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MiniPlayer() {
+    Text(text = "Mini Player")
+}
