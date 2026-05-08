90f990b7446a55bb087f6da9057529d6b0999274
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:25:01 +0600

    Add SongItem component

---
+++ app/src/main/java/com/senku/musicplayer/ui/components/SongItem.kt
@@ -0,0 +1,9 @@
+package com.senku.musicplayer.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SongItem(title: String) {
+    Text(text = title)
+}
