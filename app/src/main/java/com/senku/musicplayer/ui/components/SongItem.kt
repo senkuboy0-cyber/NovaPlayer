9eae80e46d5ff97e97699af151357270660993de
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:51:22 +0600

    Add modern song item UI

--- app/src/main/java/com/senku/musicplayer/ui/components/SongItem.kt
+++ app/src/main/java/com/senku/musicplayer/ui/components/SongItem.kt
@@ -1,9 +1,51 @@
 package com.senku.musicplayer.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.senku.musicplayer.data.model.Song
 
 @Composable
-fun SongItem(title: String) {
-    Text(text = title)
+fun SongItem(
+    song: Song,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp)
+            .clickable {
+                onClick()
+            },
+        shape = RoundedCornerShape(18.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0x22FFFFFF)
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = song.title,
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = song.artist,
+                color = Color.LightGray,
+                fontSize = 14.sp
+            )
+        }
+    }
 }
