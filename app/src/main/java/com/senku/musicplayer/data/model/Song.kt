99d2270a1bd4c74680d866776c37f9cec49701bc
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:21:57 +0600

    Add Song model

---
+++ app/src/main/java/com/senku/musicplayer/data/model/Song.kt
@@ -0,0 +1,13 @@
+package com.senku.musicplayer.data.model
+
+import android.net.Uri
+
+data class Song(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val album: String,
+    val duration: Long,
+    val uri: Uri,
+    val albumArt: Uri?
+)
