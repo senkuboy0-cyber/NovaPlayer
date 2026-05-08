b1e334b86fa3377a80d056b75766a53beb2254f9
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:38:07 +0600

    Fix MusicService abstract implementation

--- app/src/main/java/com/senku/musicplayer/player/MusicService.kt
+++ app/src/main/java/com/senku/musicplayer/player/MusicService.kt
@@ -1,5 +1,33 @@
 package com.senku.musicplayer.player
 
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.MediaSession.ControllerInfo
 
-class MusicService : MediaSessionService()
+class MusicService : MediaSessionService() {
+
+    private var mediaSession: MediaSession? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val player = ExoPlayer.Builder(this).build()
+
+        mediaSession = MediaSession.Builder(this, player).build()
+    }
+
+    override fun onGetSession(controllerInfo: ControllerInfo): MediaSession? {
+        return mediaSession
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+
+        super.onDestroy()
+    }
+}
