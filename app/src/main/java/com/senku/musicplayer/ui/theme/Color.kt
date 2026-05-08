36e27ace58c499fb524684fe2aadcc218226a413
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-08 10:43:28 +0600

    Add modern theme colors

---
+++ app/src/main/java/com/senku/musicplayer/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.senku.musicplayer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background = Color(0xFF0B0B0F)
+val Surface = Color(0xFF121218)
+val Primary = Color(0xFF7C4DFF)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFFB3B3B3)
